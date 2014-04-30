d915876c0736683800aa1206f7f1eabdca3010e9
doofmars <jan-preuss@gmx.de>
2014-04-30 19:44:47 +0200

    Prepared Base App
    Removed Uneccesary items
    Added NetworkCore

--- Anybeam/src/de/hfu/anybeam/ActivityMain.java
+++ Anybeam/src/de/hfu/anybeam/ActivityMain.java
@@ -1,100 +1,167 @@
 package de.hfu.anybeam;
 
-import de.hfu.anybeam.fragment.NavigationDrawerFragment;
-import de.hfu.anybeam.fragment.PlaceholderFragment;
-import android.app.Activity;
-import android.app.ActionBar;
-import android.app.FragmentManager;
+import de.hfu.anybeam.networkCore.Client;
+import de.hfu.anybeam.networkCore.NetworkEnvironment;
+import de.hfu.anybeam.networkCore.NetworkEnvironmentListener;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
+import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.Window;
-import android.support.v4.widget.DrawerLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ActivityMain extends Activity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class ActivityMain extends Activity implements NetworkEnvironmentListener {
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
+	private TextView console;
+	private TextView clientCount;
+	private final String GROUP_NAME = "my_group";
+	private int counter = 0;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		this.clientCount = (TextView) ActivityMain.this.findViewById(R.id.clientCount);
+		this.console = (TextView) ActivityMain.this.findViewById(R.id.console);
+		this.clientCount.setText("--");
+		this.console.setText("--");
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.global, menu);
+	    return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		if(item.getItemId() == R.id.action_refresh) {
+			NetworkEnvironment.getNetworkEnvironment(this.GROUP_NAME).startClientSearch();
+			return true;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
 
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-        setProgressBarIndeterminateVisibility(false);
-        setContentView(R.layout.activity_main);
-        
-
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
-
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
-    }
-
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(mTitle, position + 1))
-                .commit();
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
-    }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
+	@Override
+	protected void onPause() {
+		super.onPause();
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.activity_main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
-    }
+		new Thread() {
+			public void run() {
+				Looper.prepare();
+				try {
+					NetworkEnvironment.getNetworkEnvironment(ActivityMain.this.GROUP_NAME).dispose();
+				} catch (Exception e) {
+					Toast.makeText(ActivityMain.this, "NetworkEnvironment disposal FAILURE", Toast.LENGTH_SHORT).show();
+					e.printStackTrace();
+				}
+			}
+		}.start();
+		
+		Toast.makeText(ActivityMain.this, "NetworkEnvironment disposal OK", Toast.LENGTH_SHORT).show();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		try {
+			NetworkEnvironment.createNetworkEnvironment(
+					this.GROUP_NAME, 1337, 1338, Build.MODEL).addNetworkEnvironmentListener(this);
+			this.updateView();
+			Toast.makeText(this, "NetworkEnvironment initialisation OK", Toast.LENGTH_SHORT).show();
+		} catch (Exception e) {
+			Toast.makeText(this, "NetworkEnvironment initialisation FAILURE", Toast.LENGTH_SHORT).show();
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void clientFound(final Client c) {
+		counter++;
+		updateView();
+		this.runOnUiThread(new Runnable() {
+			
+			@Override
+			public void run() {
+//				Toast.makeText(MainActivity.this, c.getName() + " found", Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+	}
+
+	@Override
+	public void clientListCleared() {
+		updateView();
+
+	}
+
+	@Override
+	public void clientLost(final Client c) {
+		updateView();
+this.runOnUiThread(new Runnable() {
+			
+			@Override
+			public void run() {
+//				Toast.makeText(MainActivity.this, c.getName() + " lost", Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+
+	}
+
+	@Override
+	public void clientUpdated(final Client c) {
+		updateView();
+
+	}
+
+	public void updateView() {
+		this.runOnUiThread(new Runnable() {
+
+			@Override
+			public void run() {
+				console.setText("");
+				clientCount.setText("Anzahl Clients: " + NetworkEnvironment.getNetworkEnvironment("my_group").getClientCount() + " | Total: " + counter);
+
+				for(Client l : NetworkEnvironment.getNetworkEnvironment("my_group").getClientList())
+					console.append(l.getName() + "\n");
+			}
+		});
+	}
+
+	@Override
+	public void clientSearchStarted() {
+		this.runOnUiThread(new Runnable() {
+					
+					@Override
+					public void run() {
+						Toast.makeText(ActivityMain.this, "Searching started", Toast.LENGTH_SHORT).show();
+						ActivityMain.this.getActionBar().setTitle("Searching...");
+					}
+				});
+		
+	}
+
+	@Override
+	public void clientSearchDone() {
+		this.runOnUiThread(new Runnable() {
+			
+			@Override
+			public void run() {
+//				Toast.makeText(MainActivity.this, "Search done", Toast.LENGTH_SHORT).show();
+				ActivityMain.this.getActionBar().setTitle(R.string.app_name);
+
+			}
+		});
+	}
+
 }
