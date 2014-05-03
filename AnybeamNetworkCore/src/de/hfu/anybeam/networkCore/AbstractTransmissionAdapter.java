37a192a91e2dc0830ed17364503be8fd93bf6b14
crysxd <c.wuerthner@me.com>
2014-05-03 11:16:07 +0200

    Started optimizing and documenting
    Created a doc folder in AnyBeamNetworkCore containing a documentation
    with public modifiers

--- AnybeamNetworkCore/src/de/hfu/anybeam/networkCore/AbstractTransmissionAdapter.java
+++ AnybeamNetworkCore/src/de/hfu/anybeam/networkCore/AbstractTransmissionAdapter.java
@@ -1,11 +1,39 @@
 package de.hfu.anybeam.networkCore;
 
-
+/**
+ * An adapter interface allowing a class to be informed about status updates of a {@link AbstractTransmission} object.
+ * @author chrwuer
+ * @version 1.0
+ * @since 1.0
+ */
 public interface AbstractTransmissionAdapter {
 
+	/**
+	 * Is called when the transmission was started successfully.
+	 * @param e the transmission event displaying the {@link AbstractTransmission} object's state
+	 */
 	public void transmissionStarted(TransmissionEvent e);
+	
+	/**
+	 * Is called when the transmission progress was updated.
+	 * @param e the transmission event displaying the {@link AbstractTransmission} object's state
+	 */
 	public void transmissionProgressChanged(TransmissionEvent e);
+	
+	/**
+	 * Is called when the transmission was done successfully.
+	 * @param e the transmission event displaying the {@link AbstractTransmission} object's state
+	 */
 	public void transmissionDone(TransmissionEvent e);
+	
+	/**
+	 * Is called when the transmission was started aborted. This can happen under two different circumstances
+	 * <ul>
+	 * 	<li>The transmission was canceled using {@link AbstractTransmission#cancelTransmission()}</li>
+	 * 	<li>An error occured while transmitting the data. In this case the method {@link TransmissionEvent#getException()} returns the exception object.</li>
+	 * </ul>
+	 * @param e the transmission event displaying the {@link AbstractTransmission} object's state
+	 */
 	public void transmissionFailed(TransmissionEvent e);
 	
 }
--- AnybeamNetworkCore/src/de/hfu/anybeam/networkCore/AverageList.java
+++ AnybeamNetworkCore/src/de/hfu/anybeam/networkCore/AverageList.java
@@ -2,25 +2,45 @@ package de.hfu.anybeam.networkCore;
 
 import java.util.Vector;
 
+/**
+ * A {@link Vector} subclass providing functionality to calculate a average value over all values included in this list.
+ * Only the last max values are stored, all others are disposed.
+ * @author chrwuer
+ * @since 1.0
+ * @version 1.0
+ */
 public class AverageList extends Vector<Double> {
-
+	
 	private static final long serialVersionUID = 7801680903484273521L;
+	
+	//The max size of the list
 	private final int MAX_SIZE;
 
+	/**
+	 * Creates a new {@link AverageList} object with the given max number of elements.
+	 * @param maxSize the max number of elements in this list
+	 */
 	public AverageList(int maxSize) {
 		this.MAX_SIZE = maxSize < 1 ? 1 : maxSize;
 	}
 
 	@Override
 	public synchronized boolean add(Double t) {
+		//Remove a element if there would be more than the max
 		if(this.size()+1 > this.MAX_SIZE) {
 			 this.remove(0);
 		}
 		
+		//Add the new value
 		return super.add(t);
 	}
 	
+	/**
+	 * Calculates the average value of all elements in this list.
+	 * @return the calculated average
+	 */
 	public synchronized double getAverage() {
+		//TODO: Use better average calculation algorithm
 		double sum = 0;
 		
 		for(Double d : this) {
