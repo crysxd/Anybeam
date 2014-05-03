2cad7e4e3a0df09ca4247007fc22985812c5f1b6
crysxd <c.wuerthner@me.com>
2014-05-03 21:33:14 +0200

    New Icons

--- Anybeam/src/de/hfu/anybeam/android/MainActivity.java
+++ Anybeam/src/de/hfu/anybeam/android/MainActivity.java
@@ -1,5 +1,11 @@
 package de.hfu.anybeam.android;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import de.hfu.anybeam.android.R;
 import de.hfu.anybeam.networkCore.Client;
 import de.hfu.anybeam.networkCore.DeviceType;
@@ -11,41 +17,44 @@ import de.hfu.anybeam.networkCore.NetworkEnvironmentSettings;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ImageSpan;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements NetworkEnvironmentListener {
 
 
 	private final String GROUP_NAME = "my_group";
-	private NetworkEnvironmentSettings SETTINGS;
+	private NetworkEnvironmentSettings SETTINGS = new NetworkEnvironmentSettings(this.GROUP_NAME, "MacBook Pro", DeviceType.TYPE_LAPTOP, 
+			EncryptionType.AES128, 1338, 1337, EncryptionUtils.generateSecretKeyFromPassword("anybeamRockt1137", EncryptionType.AES128));;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		NetworkEnvironmentSettings set = null;
-		try {
-			set = new NetworkEnvironmentSettings(GROUP_NAME, Build.MODEL, DeviceType.TYPE_SMARPHONE, 
-					EncryptionType.AES128, 1338, 1337,  EncryptionUtils.generateSecretKey(EncryptionType.AES128), "Android");
-		} catch(Exception e) {
-			e.printStackTrace();
-			set = new NetworkEnvironmentSettings(GROUP_NAME, Build.MODEL, DeviceType.TYPE_SMARPHONE, 
-					EncryptionType.AES128, 1338, 1337,  new byte[0], "Android");
-		}
-		
-		this.SETTINGS = set;
-			
+
 		setContentView(R.layout.activity_main);
+
+		TextView tv = (TextView) this.findViewById(R.id.textView2);
+		CharSequence text = this.getResources().getString(R.string.main_instruction);
+		text = this.addSmileySpans(text);
+		tv.setText(text);
+		
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-		
+
 		try {
 			NetworkCoreUtils.createNetworkEnvironment(this.SETTINGS).addNetworkEnvironmentListener(this);
 		} catch (Exception e) {
@@ -55,19 +64,19 @@ public class MainActivity extends Activity implements NetworkEnvironmentListener
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.main_menu, menu);
-	    return true;
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_menu, menu);
+		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		
+
 		if(item.getItemId() == R.id.action_settings) {
 			Toast.makeText(this, "Setting", Toast.LENGTH_LONG).show();
 			return true;
 		}
-		
+
 		return super.onOptionsItemSelected(item);
 	}
 
@@ -80,39 +89,51 @@ public class MainActivity extends Activity implements NetworkEnvironmentListener
 		startActivity(clipboardIntent);
 	}
 
+	private CharSequence addSmileySpans(CharSequence text) {
+		//smilyRegexMap = new HashMap<Integer, String>();
+
+		String indicator = "shareicon";
+		SpannableStringBuilder builder = new SpannableStringBuilder(text);
+		Bitmap smiley = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_action_share);
+		int start = text.toString().indexOf(indicator);
+		builder.setSpan(new ImageSpan(smiley, ImageSpan.ALIGN_BASELINE), start,start+indicator.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+		return builder;
+	}
+
 	@Override
 	public void clientFound(Client c) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void clientUpdated(Client c) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void clientLost(Client c) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void clientListCleared() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void clientSearchStarted() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void clientSearchDone() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
