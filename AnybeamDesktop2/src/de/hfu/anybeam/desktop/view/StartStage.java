14177650bc1353919b024e8769eb1fd3623424c9
crysxd <c.wuerthner@me.com>
2014-06-03 23:48:07 +0200

    Added "Beam Screenshot" option

--- AnybeamDesktop2/src/de/hfu/anybeam/desktop/view/StartStage.java
+++ AnybeamDesktop2/src/de/hfu/anybeam/desktop/view/StartStage.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.border.EmptyBorder;
 
+import de.hfu.anybeam.desktop.view.androidUI.ActionbarButton;
 import de.hfu.anybeam.desktop.view.androidUI.AndroidUI;
 import de.hfu.anybeam.desktop.view.androidUI.Stage;
 import de.hfu.anybeam.desktop.view.resources.R;
@@ -18,8 +19,8 @@ public class StartStage extends Stage implements ActionListener {
 	private final JButton SEND_CLIPBOARD_BUTTON = new BigButton("Beam Clipboard", R.getImage("ic_action_send_clipboard.png"));;
 	private final JButton SEND_FILE_BUTTON = new BigButton("Beam File", R.getImage("ic_action_send_file.png"));
 	private final JButton SEND_TEXT_BUTTON = new BigButton("Beam Text", R.getImage("ic_action_edit.png"));
-	private final JButton SETTINGS_BUTTON = new BigButton("Settings", R.getImage("ic_action_settings.png"));
-//	private final ActionbarButton SETTINGS_BUTTON = new ActionbarButton(R.getImage("ic_action_settings.png"));
+	private final JButton SEND_SCREENSHOT_BUTTON = new BigButton("Beam Screenshot", R.getImage("ic_action_camera.png"));
+	private final ActionbarButton SETTINGS_BUTTON = new ActionbarButton(R.getImage("ic_action_settings.png"));
 
 	private final SettingsStage SETTINGS_STAGE;
 	private final SendStage SEND_STAGE;
@@ -32,7 +33,7 @@ public class StartStage extends Stage implements ActionListener {
 		this.SEND_STAGE = new SendStage(this);
 		
 		//Add Settings action and add ActionListener
-//		this.addAction(this.SETTINGS_BUTTON);
+		this.addAction(this.SETTINGS_BUTTON);
 	
 		
 		//Border and Layout
@@ -43,12 +44,13 @@ public class StartStage extends Stage implements ActionListener {
 		this.add(this.SEND_CLIPBOARD_BUTTON);
 		this.add(this.SEND_FILE_BUTTON);
 		this.add(this.SEND_TEXT_BUTTON);
-		this.add(this.SETTINGS_BUTTON);
+		this.add(this.SEND_SCREENSHOT_BUTTON);
 		
 		//Add ActionListener
 		this.SEND_CLIPBOARD_BUTTON.addActionListener(this);
 		this.SEND_FILE_BUTTON.addActionListener(this);
 		this.SEND_TEXT_BUTTON.addActionListener(this);
+		this.SEND_SCREENSHOT_BUTTON.addActionListener(this);
 		this.SETTINGS_BUTTON.addActionListener(this);
 	}
 	
@@ -86,6 +88,13 @@ public class StartStage extends Stage implements ActionListener {
 			this.getAndroidUI().enterStage(this.SEND_STAGE);
 			//TODO set send content
 		}
+		
+		//If the start stage's send screenshot button was pressed -> go to send stage
+		if(e.getSource() == this.SEND_SCREENSHOT_BUTTON) {
+			this.getAndroidUI().enterStage(this.SEND_STAGE);
+			//TODO set send content
+		}
+
 
 		//If the start stage's send file button was pressed -> go to send stage
 		if(e.getSource() == this.SEND_FILE_BUTTON) {
