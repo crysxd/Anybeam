a887a1340f34e7a42247e2dacf73d1d92e9fad43
doofmars <jan-preuss@gmx.de>
2014-06-07 00:40:43 +0200

    Small bugfixes
    Notifications Display
    UTF-8 transmission
    Landscape Only

--- AnybeamDesktop2/src/de/hfu/anybeam/desktop/model/ClipboardUtils.java
+++ AnybeamDesktop2/src/de/hfu/anybeam/desktop/model/ClipboardUtils.java
@@ -7,6 +7,7 @@ public class ClipboardUtils {
 	
 	public static InputStream getClipboardContentAsStream() {
 		//TODO return real value! If value != text -> return null
+		//TODO return Byte Array in utf-8 format
 		return new ByteArrayInputStream("Hello World".getBytes());
 //		return null;
 	}
